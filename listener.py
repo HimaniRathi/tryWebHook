0b5495e66d2cc972b5df188842d0d52793a968b2
HIMANI <himani.rathi@stanzaliving.com>
2019-07-26 19:28:35 +0530

    final code probably

--- listener.py
+++ listener.py
@@ -1,6 +1,7 @@
 import sys 
 from flask import Flask, request, abort
 import json
+import os
 
 app = Flask(__name__)
 
@@ -9,18 +10,19 @@ def webhook():
   print("webhook"); sys.stdout.flush()
   if request.method == 'POST':
     print(request.json)
+    os.system('bash script.sh')
     # updateVersion()
     return '', 200
   else:
     abort(400)
 
-def updateVersion():
-  data = {}
-  with open("version.json","r") as f:
-    data = json.loads(f.read())
-  data["version"] = data["version"] + 1
-  with open("version.json","w") as f:
-    f.write(json.dumps(data))
+# def updateVersion():
+#   data = {}
+#   with open("version.json","r") as f:
+#     data = json.loads(f.read())
+#   data["version"] = data["version"] + 1
+#   with open("version.json","w") as f:
+#     f.write(json.dumps(data))
 
 if __name__ == '__main__':
   app.run()
